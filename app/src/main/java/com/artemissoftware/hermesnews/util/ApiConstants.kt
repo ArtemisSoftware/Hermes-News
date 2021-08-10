b86886f8c41476cc313f5f3e9dee3556e724bf12
gustavo <gustavo.isaac@gmail.com>
2021-08-10 13:17:47 +0100

    News api & constants

---
+++ app/src/main/java/com/artemissoftware/hermesnews/util/ApiConstants.kt
@@ -0,0 +1,9 @@
+package com.artemissoftware.hermesnews.util
+
+class ApiConstants {
+
+    companion object {
+        const val API_KEY = "6953a7421bb749c589e687746c85eee8"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
