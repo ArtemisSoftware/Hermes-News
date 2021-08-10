5ae88416d05314d529d127efe4bc26d92f8bada5
gustavo <gustavo.isaac@gmail.com>
2021-08-10 12:43:54 +0100

    BreakingNewsFragment

--- app/src/main/java/com/artemissoftware/hermesnews/ui/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/hermesnews/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.artemissoftware.hermesnews
+package com.artemissoftware.hermesnews.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.artemissoftware.hermesnews.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
