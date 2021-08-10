2bb75bc3532cafe356223dffcff736c014be2793
gustavo <gustavo.isaac@gmail.com>
2021-08-10 12:45:33 +0100

    SavedNewsFragment

---
+++ app/src/main/java/com/artemissoftware/hermesnews/ui/fragments/SavedNewsFragment.kt
@@ -0,0 +1,11 @@
+package com.artemissoftware.hermesnews.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.artemissoftware.hermesnews.R
+
+class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
+}
