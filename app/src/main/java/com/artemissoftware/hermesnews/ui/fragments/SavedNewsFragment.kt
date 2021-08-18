d7744a3016c424a97690e8c1932dc39e7146561d
gustavo <gustavo.isaac@gmail.com>
2021-08-18 18:31:44 +0100

    Delete saved articles

--- app/src/main/java/com/artemissoftware/hermesnews/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/artemissoftware/hermesnews/ui/fragments/SavedNewsFragment.kt
@@ -7,10 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.artemissoftware.hermesnews.R
 import com.artemissoftware.hermesnews.ui.MainActivity
 import com.artemissoftware.hermesnews.ui.fragments.adapters.NewsAdapter
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_saved_news.*
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
@@ -24,6 +27,35 @@ class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
         setupRecyclerView()
         setupClickListeners()
+
+
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(
+            ItemTouchHelper.UP or ItemTouchHelper.DOWN,
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+        ) {
+            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                val position = viewHolder.adapterPosition
+                val article = newsAdapter.differ.currentList[position]
+                viewModel.deleteArticle(article)
+
+                Snackbar.make(view, getString(R.string.successfully_deleted_article), Snackbar.LENGTH_LONG).apply {
+                    setAction(getString(R.string.undo)) {
+                        viewModel.saveArticle(article)
+                    }
+                    show()
+                }
+            }
+        }
+
+        ItemTouchHelper(itemTouchHelperCallback).apply {
+            attachToRecyclerView(rvSavedNews)
+        }
 
 
         viewModel.getSavedNews().observe(viewLifecycleOwner, Observer { articles ->
